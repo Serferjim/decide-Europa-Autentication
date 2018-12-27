1151702720716c1ad3a965dc60ba54023023a46d
miguel_campos_romero <camposromeromiguel@hotmail.com>
2018-12-27 15:13:39 +0100

    Conflictos arreglados

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'axes',
     'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
